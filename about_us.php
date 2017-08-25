3c68bc3ab1d16120e7cd621b415b316ed5932447
mayomi <mayomi@sublime.lk>
2017-08-25 14:25:51 +0530

    add <p> tag

--- about_us.php
+++ about_us.php
@@ -6,14 +6,16 @@
     </head>
     <body>
         <div>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam corporis, error fugit
-            maiores nostrum similique voluptate. Autem delectus doloremque dolores earum eius expedita
-            fugiat id non perferendis quibusdam quidem quos sapiente tempore tenetur unde, voluptas
-            voluptatum? Ab debitis dolorum enim harum illum incidunt itaque nostrum quam sint. Iusto
-            laborum pariatur sint sunt ut velit voluptate! Amet autem, blanditiis deleniti distinctio
-            eum excepturi ipsa iure magnam magni minima nemo nisi nulla obcaecati omnis perferendis
-            possimus provident quasi saepe velit voluptatem. Assumenda consectetur et impedit laboriosam
-            minima necessitatibus nobis recusandae repudiandae!
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam corporis, error fugit
+                maiores nostrum similique voluptate. Autem delectus doloremque dolores earum eius expedita
+                fugiat id non perferendis quibusdam quidem quos sapiente tempore tenetur unde, voluptas
+                voluptatum? Ab debitis dolorum enim harum illum incidunt itaque nostrum quam sint. Iusto
+                laborum pariatur sint sunt ut velit voluptate! Amet autem, blanditiis deleniti distinctio
+                eum excepturi ipsa iure magnam magni minima nemo nisi nulla obcaecati omnis perferendis
+                possimus provident quasi saepe velit voluptatem. Assumenda consectetur et impedit laboriosam
+                minima necessitatibus nobis recusandae repudiandae!
+            </p>
         </div>
     </body>
 </html>
