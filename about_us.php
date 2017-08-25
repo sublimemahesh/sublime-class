af645824db156ff0f6f4540bea937e472ef91a60
mayomi <mayomi@sublime.lk>
2017-08-25 14:14:45 +0530

    #11 Add some details

--- about_us.php
+++ about_us.php
@@ -5,8 +5,15 @@
         <title>About Us | Git Class</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aperiam corporis, error fugit
+            maiores nostrum similique voluptate. Autem delectus doloremque dolores earum eius expedita
+            fugiat id non perferendis quibusdam quidem quos sapiente tempore tenetur unde, voluptas
+            voluptatum? Ab debitis dolorum enim harum illum incidunt itaque nostrum quam sint. Iusto
+            laborum pariatur sint sunt ut velit voluptate! Amet autem, blanditiis deleniti distinctio
+            eum excepturi ipsa iure magnam magni minima nemo nisi nulla obcaecati omnis perferendis
+            possimus provident quasi saepe velit voluptatem. Assumenda consectetur et impedit laboriosam
+            minima necessitatibus nobis recusandae repudiandae!
+        </div>
     </body>
 </html>
